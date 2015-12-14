18354a002d7f2fa0570fcd8a1e948e1edcb21868
Esther Jun Kim <esther.jun.kim@gmail.com>
2015-12-13 16:13:58 -0800

    Add ability to sync switch states

--- app/src/main/java/com/drejkim/ledimoteandroid/MainActivity.java
+++ app/src/main/java/com/drejkim/ledimoteandroid/MainActivity.java
@@ -1,10 +1,13 @@
 package com.drejkim.ledimoteandroid;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Switch redSwitch;
+        final Switch redSwitch;
         redSwitch = (Switch) findViewById(R.id.switch_red);
         redSwitch.setChecked(false);
         redSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Switch greenSwitch;
+        final Switch greenSwitch;
         greenSwitch = (Switch) findViewById(R.id.switch_green);
         greenSwitch.setChecked(false);
         greenSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -50,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Switch blueSwitch;
+        final Switch blueSwitch;
         blueSwitch = (Switch) findViewById(R.id.switch_blue);
         blueSwitch.setChecked(false);
         blueSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -60,13 +63,132 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Switch yellowSwitch;
+        final Switch yellowSwitch;
         yellowSwitch = (Switch) findViewById(R.id.switch_yellow);
         yellowSwitch.setChecked(false);
         yellowSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 sendSwitchState("yellow", isChecked);
+            }
+        });
+
+        mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                mSocket.emit("hello");
+            }
+
+        });
+
+        mSocket.on("init", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                final JSONObject data = (JSONObject) args[0];
+
+                Log.d(TAG, data.toString());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            redSwitch.setChecked((Boolean) data.get("red"));
+                            greenSwitch.setChecked((Boolean) data.get("green"));
+                            blueSwitch.setChecked((Boolean) data.get("blue"));
+                            yellowSwitch.setChecked((Boolean) data.get("yellow"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        mSocket.on("red", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                final JSONObject data = (JSONObject) args[0];
+
+                Log.d(TAG, data.toString());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            redSwitch.setChecked((Boolean) data.get("state"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        mSocket.on("green", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                final JSONObject data = (JSONObject) args[0];
+
+                Log.d(TAG, data.toString());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            greenSwitch.setChecked((Boolean) data.get("state"));
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        mSocket.on("blue", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                final JSONObject data = (JSONObject) args[0];
+
+                Log.d(TAG, data.toString());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            blueSwitch.setChecked((Boolean) data.get("state"));
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        mSocket.on("yellow", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                final JSONObject data = (JSONObject) args[0];
+
+                Log.d(TAG, data.toString());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            yellowSwitch.setChecked((Boolean) data.get("state"));
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         });
 
